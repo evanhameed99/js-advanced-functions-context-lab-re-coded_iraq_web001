bbec32026d4d5f2f8c2e8f16320bfcd29147fd2b
Evan hameed <evanhameed9966@gmail.com>
2020-06-13 05:21:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function createEmployeeRecord (array){
   return {
     firstName : array[0],
     familyName : array[1],
-    title : array[3],
-    payPerHour : array[2],
+    title : array[2],
+    payPerHour : array[3],
     timeInEvents : [],
     timeOutEvents : []
 
